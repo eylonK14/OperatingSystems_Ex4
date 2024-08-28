583afd128aeb4e8db62c14cd9dcfaea07c85139f
eylonK14 <eylony1403@gmail.com>
2024-08-28 22:02:06 +0300

    finished Ex5

--- hello.cpp
+++ hello.cpp
@@ -1,18 +1,20 @@
 /*
  * File: hello.c
  */
+// problem #1 - no inclusion for <stdio.h> while trying to use printf
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-    char *string, *string_so_far;
+    char *string = 0, *string_so_far = 0; // problem #4 - string is not initialized
     int i, length;
     length = 0;
     for (i = 0; i < argc; i++)
     {
         length += strlen(argv[i]) + 1;
-        string = malloc(length + 1);
+        string = /* problem #2 - add casting/*/ (char *)malloc(length + 1);
 
         /*  * Copy the string built so far. */
         if (string_so_far != (char *)0)
@@ -22,8 +24,10 @@ int main(int argc, char *argv[])
         strcat(string, argv[i]);
         if (i < argc - 1)
             strcat(string, " ");
+        free(string_so_far); // problem #5 - needed to free string_so_far
         string_so_far = string;
     }
     printf("You entered: %s\n", string_so_far);
+    free(string); // problem #3 - memory leak
     return (0);
 }
--- makefile
+++ makefile
@@ -2,7 +2,10 @@ CC = g++
 CFLAGS = -pg -g -Wall -Werror -Wextra
 OBJS = hierholzers.o EulerianCycleFinder.o
 
-all: hierholzers
+all: hierholzers hello
+
+hello: hello.o
+	$(CC) $(CFLAGS) -o $@ $^
 
 hierholzers: $(OBJS)
 	$(CC) $(CFLAGS) -o $@ $^
@@ -10,7 +13,8 @@ hierholzers: $(OBJS)
 *.o: *.cpp
 	$(CC) $(CFLAGS) -c $<
 
-clean:
-	rm -f hierholzers *.o gmon.out *.gcov *.gcda *.gcno
-
 .PHONY: clean all
+
+clean:
+	rm -f hierholzers hello *.o gmon.out *.gcov *.gcda *.gcno
+
